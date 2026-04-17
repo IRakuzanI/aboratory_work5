397c461ed9d76ca797fd73a52f4f530a001c3d46
Нартикоев Батраз Витальевич <125532794+IRakuzanI@users.noreply.github.com>
2026-04-17 22:38:30 +0300

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    db_name = os.environ.get('DB_NAME', 'unknown')
+    return jsonify({
+        "status": "success",
+        "message": "Backend успешно запущен",
+        "database": db_name
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
