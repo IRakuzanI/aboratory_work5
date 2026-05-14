7541ccf3370a719e5d565f827b72120290075add
IvanPantuhin <pantiukhin.ia@phystech.edu>
2026-05-14 15:29:08 +0300

    update backend

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,16 @@
 import os
 from flask import Flask, jsonify
 
-
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def api_root():
-    db_name = os.environ.get('DB_NAME', 'unknown')
-    return jsonify({
-        "status": "success",
-        "message": "Backend успешно запущен",
-        "database": db_name
-    })
+    db_name = os.environ.get("DB_NAME", "unknown")
+    return jsonify(
+        {"status": "success", "message": "Backend успешно запущен", "database": db_name}
+    )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
